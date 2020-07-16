da7f0d3720fa91abadd45b4fd44b8eee1bae2b7f
Christina Riachi <christina.riachi@mail.mcgill.ca>
2020-07-16 18:20:53 -0400

    added small comment

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
 // import built-in Node package
-const http = require('http');
+const http = require("http");
 const port = 4000;
 
-const server = http.createServer((req, res) => { // Callback function in ES6
-	// Response header
-	res.writeHead(200, { "Content-Type": "application/json" });
+const server = http.createServer((req, res) => {
+  // Callback function in ES6
+  // Response header
+  res.writeHead(200, { "Content-Type": "application/json" });
 
-	// JSON object
-	const song = {
-		id: 12345,
-		favorite: false,
-		title: "Hello World",
-		artist: "Node programmer",
-		album: "Node EP"
-	};
+  // JSON object
+  const song = {
+    id: 12345,
+    favorite: false,
+    title: "Hello World",
+    artist: "Node programmer",
+    album: "Node EP",
+  };
 
-	console.log(`${song.title} by ${song.artist}`);
+  console.log(`${song.title} by ${song.artist}`);
 
-	// send JSON response to client
-	res.end(JSON.stringify(song));      // JSON.stringify({a: 1}) -> '{"a":1}'
+  // send JSON response to client
+  res.end(JSON.stringify(song)); // JSON.stringify({a: 1}) -> '{"a":1}' apparently it serializes the data
 });
 
-server.listen(port, () => { // Callback function in ES6
-	console.log(`Server listening at ${port}`);
+server.listen(port, () => {
+  // Callback function in ES6
+  console.log(`Server listening at ${port}`);
 });
