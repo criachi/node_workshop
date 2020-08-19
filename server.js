a794dff903ffd1c9d5270d9c25e34785ffc64fd6
Christina <christina.riachi@mail.mcgill.ca>
2020-08-18 21:16:10 -0400

    added explanatory comments

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // import built-in Node packages
-const express = require('express'); // import express
+const express = require("express"); // import express
 const server = express();
 
 const port = 4000;
 
 // HTML routes
+// syntax/format: server.http_verb(route, route_handler)
+// route handler is the function to call to serve the client's request
 server.get("/", (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + "/index.html");
 });
 
 // JSON routes
 server.get("/json", (req, res) => {
-	res.send((JSON.stringify({ name: "Lenny" })));
+  res.send(JSON.stringify({ name: "Lenny" }));
 });
 
-server.listen(port, () => { // Callback function in ES6
-	console.log(`Server listening at ${port}`);
+server.listen(port, () => {
+  // Callback function in ES6
+  console.log(`Server listening at ${port}`);
 });
